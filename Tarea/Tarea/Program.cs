858b02fdc4fe668de42380f556d62942e355158e
erickslk <erick.osorto@unitec.edu>
2023-01-27 00:08:56 -0600

    tercero

--- Tarea/Tarea/Program.cs
+++ Tarea/Tarea/Program.cs
@@ -2,11 +2,25 @@
 using System.Security.Cryptography.X509Certificates;
 using Tarea.Models;
 
-Expresion expresion = new Expresion();
+Expresion expresion1 = new Expresion()
 {
-    expresion.num1 = 5;
-    expresion.num2 = 6;
+    num1 = 5,
+    num2 = 6,
 
+};
+
+Expresion expresion2 = new Expresion()
+{
+    num1 = 10,
+    num2 = 15,
+};
+
+List<Expresion> ListaExpresiones = new List<Expresion>();
+ListaExpresiones.Add(expresion1);
+ListaExpresiones.Add(expresion2);
+    
+   foreach(Expresion x in ListaExpresiones)
+{
+
+    Console.WriteLine(x.toString());
 }
-
-Console.WriteLine(expresion.toString());
